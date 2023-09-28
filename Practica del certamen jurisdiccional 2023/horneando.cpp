5894ce1aee4d052fa95968cff132faad41720a6b
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2023-09-28 11:11:45 -0300

    agregado: cambios en el archivo horneando.cpp para que pase las pruevas

--- Practica del certamen jurisdiccional 2023/horneando.cpp
+++ Practica del certamen jurisdiccional 2023/horneando.cpp
@@ -1,8 +1,4 @@
-#include<iostream>
-
 //Enunciado dado en la practica del certamen jurisdiccional 2023
-
-/* NO PASA LAS PRUEVAS DEL JUEZ */
 
 void alarma();
 
@@ -11,16 +7,11 @@ struct hornoBase {
 } horno = { 0 };
 
 void inicializar(int temperaturaMaxima) {
-	if(temperaturaMaxima >= 200) {
-		horno.temperaturaMaxima = temperaturaMaxima;
-	}
+	horno.temperaturaMaxima = temperaturaMaxima;
 }
 
 void cambiarTemperatura(int nuevaTemperatura) {
 	if(nuevaTemperatura > horno.temperaturaMaxima) {
 		alarma();
-		std::cout<<"suena la alarma\n";
-	} else std::cout<<"no suena la alarma\n";
+	} 
 }
-
-
